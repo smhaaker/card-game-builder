8d509178567ac365967278775c0e59f68455b682
Steffen <steffen.haaker@gmail.com>
2019-10-28 00:01:32 -0400

    Added gameplay features to gods game

--- examples/gods/js/script.js
+++ examples/gods/js/script.js
@@ -1,3 +1,11 @@
+// player 1 Defs:
+let healthPlayer1 = 100;
+let actionsPlayer1 = 3;
+
+// player 2 Defs:
+let healthPlayer2 = 100;
+let actionsPlayer2   = 3;
+
 // Player 1 define deck
 let zeus = createCardSet(2, "Zeus", 50, "red", ['thunderbolt'], 'Zeus is the king of the gods', 'https://www.ancient-origins.net/sites/default/files/field/image/Unshakeable-Power-of-Zeus.jpg')
 let cassandra = createCardSet(4, "Cassandra", 5, "red", ['predict'], 'Cassandra predicts the future')
@@ -29,9 +37,16 @@ let shuffledPlayer2 = shuffle(deckPlayer2);
 function dealAndShowPlayer1() {
     let cardsDealt = deal(shuffledPlayer1, 5, 1)
     console.log(cardsDealt)
+
     
+    let playerHealthInfo = document.getElementById('player1Health')
+    playerHealthInfo.innerHTML = `Health ${healthPlayer1}`
+    let playerMovesInfo = document.getElementById('player1Moves')
+    playerMovesInfo.innerHTML = `Actions: ${actionsPlayer1}`
     let cardsOutput = document.getElementById('cardsInHand')
-    
+    let cardsLeftInDeck = document.getElementById('player1deckcards')
+    cardsLeftInDeck.innerHTML = shuffledPlayer1.length - cardsDealt.length
+
     cardsOutput.innerHTML = ''
     for (let i = 0; i < cardsDealt.length; i++)
     {
@@ -43,30 +58,43 @@ function dealAndShowPlayer1() {
             </div>
             <div id="descSmall">Desc: ${cardsDealt[i][0].description}</div>
             <div id="abilitiesSmall">Abilities: ${cardsDealt[i][0].abilities}</div>
-            CardID: ${cardsDealt[i][0].id}
-
-            <div class="valueSmall" id="valueSmall${i}">
+              <div class="valueSmall" id="valueSmall${i}">
                 ${cardsDealt[i][0].value} <br/>
             </div>
         </div>`
         // cardsOutput.innerHTML += cardsDealt[i][0].cardName
     }
+
 }
 
 function playCard(id){
-    console.log(id)
-    $(`#cardShow${id}`).appendTo("#player1Board");
-    let cardId = document.getElementById(`#cardShow${id}`)
-    $(`#cardShow${id}`).attr('id',`id_new${id}`);
-    // cardsInHand
+    if (actionsPlayer1 <= 0){
+        console.log('no moves left!')
+    }
+    else {
+        console.log(id)
+        $(`#cardShow${id}`).appendTo("#player1Board");
+        let cardId = document.getElementById(`#cardShow${id}`)
+        $(`#cardShow${id}`).attr('id',`id_new${id}`);
+        // cardsInHand
+        actionsPlayer1--;
+        let playerMovesInfo = document.getElementById('player1Moves')
+        playerMovesInfo.innerHTML = `Actions: ${actionsPlayer1}`
+    }
 }
 
 function dealAndShowPlayer2() {
     let cardsDealt = deal(shuffledPlayer2, 5, 1)
     console.log(cardsDealt)
     
+        
+    let playerHealthInfo = document.getElementById('player2Health')
+    playerHealthInfo.innerHTML = `Health: ${healthPlayer2}`
+    let playerMovesInfo = document.getElementById('player2Moves')
+    playerMovesInfo.innerHTML = `Actions: ${actionsPlayer2}`
     let cardsOutput = document.getElementById('cardsInHandPlayer2')
-    
+    let cardsLeftInDeck = document.getElementById('player2deckcards')
+    cardsLeftInDeck.innerHTML = shuffledPlayer2.length - cardsDealt.length
     cardsOutput.innerHTML = ''
     for (let i = 0; i < cardsDealt.length; i++)
     {
@@ -78,8 +106,6 @@ function dealAndShowPlayer2() {
             </div>
             <div id="descSmall">Desc: ${cardsDealt[i][0].description}</div>
             <div id="abilitiesSmall">Abilities: ${cardsDealt[i][0].abilities}</div>
-            CardID: ${cardsDealt[i][0].id}
-
             <div class="valueSmall" id="valueSmall${i}">
                 ${cardsDealt[i][0].value} <br/>
             </div>
@@ -89,11 +115,19 @@ function dealAndShowPlayer2() {
 }
 
 function playCardPlayer2(id){
-    console.log(id)
-    $(`#cardShowPlayer2${id}`).appendTo("#player2Board");
-    let cardId = document.getElementById(`#cardShowPlayer2${id}`)
-    $(`#cardShowPlayer2${id}`).attr('id',`id_new${id}`);
-    // cardsInHand
+    if (actionsPlayer2 <= 0){
+        console.log('no moves left!')
+    }
+    else {
+        console.log(id)
+        $(`#cardShowPlayer2${id}`).appendTo("#player2Board");
+        let cardId = document.getElementById(`#cardShowPlayer2${id}`)
+        $(`#cardShowPlayer2${id}`).attr('id',`id_new${id}`);
+        // cardsInHand
+        actionsPlayer2--;
+        let playerMovesInfo = document.getElementById('player2Moves')
+        playerMovesInfo.innerHTML = `Actions: ${actionsPlayer2}`
+    }
 }
 
 function dealCards() {
